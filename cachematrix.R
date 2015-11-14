80884b619bdecd873dbd6850043a603ff6739b15
Emanuele <pellichero@gmail.com>
2015-11-14 14:48:35 -0800

    assignment 2 complete

--- cachematrix.R
+++ cachematrix.R
@@ -12,10 +12,10 @@
 ## 3.set the value of the inverse of the matrix
 ## 4.get the value of the inverse of the matrix
 
-## The second function, cacheSolve calculates the mean of the special "vector" created with makeCacheMatrix. 
-## However, it first checks to see if the mean has already been calculated. If so, it gets the mean from the 
-## cache and skips the computation. Otherwise, it calculates the mean of the data and sets the value of the 
-## mean in the cache via the setmean function
+## The second function, cacheSolve calculates the inverse of the special "matrix" created with makeCacheMatrix. 
+## However, it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the 
+## cache and skips the computation. Otherwise, it calculates the inverse of the data and sets the value of the 
+## inverse in the cache via the setinverse function
 
 
 ## THe makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
