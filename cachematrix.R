81c0742e8a17ffa0cd6484ea3712c21eacde5eca
Emanuele <pellichero@gmail.com>
2015-11-14 14:44:14 -0800

    assignment 2 complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## JH Data Science Certificate - R Programming Course - Week 3
+## Assignment 2: Caching the Inverse of a Matrix
+## Emanuele Pellichero
 
-## Write a short comment describing this function
+## The pair of functions below are used to cache the inverse of a matrix.
+
+## The first function, makeCacheMatriX creates a special "matrix", which is really a list containing a 
+## function to:
+
+## 1.set the value of the matrix
+## 2.get the value of the matrix
+## 3.set the value of the inverse of the matrix
+## 4.get the value of the inverse of the matrix
+
+## The second function, cacheSolve calculates the mean of the special "vector" created with makeCacheMatrix. 
+## However, it first checks to see if the mean has already been calculated. If so, it gets the mean from the 
+## cache and skips the computation. Otherwise, it calculates the mean of the data and sets the value of the 
+## mean in the cache via the setmean function
+
+
+## THe makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
+
